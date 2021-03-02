8f4e54f911971717c51abc35e8457991e662f820
Riza Alif Wildani <rizaalifdmc@gmail.com>
2021-03-02 13:43:10 +0700

    Eslint Setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,29 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
-};
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+	parserOptions: {
+		parser: "babel-eslint"
+	},
+	rules: {
+		semi: [2, "never"],
+		quotes: ["error", "double"],
+		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		"prettier/prettier": ["error", { semi: false }]
+	},
+	overrides: [
+		{
+			files: [
+				"**/__tests__/*.{j,t}s?(x)",
+				"**/tests/unit/**/*.spec.{j,t}s?(x)"
+			],
+			env: {
+				jest: true
+			}
+		}
+	]
+}
